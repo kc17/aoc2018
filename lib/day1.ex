f94288ed7fc220551fd5f49e073c791238a3eb38
Mickey <poverwhelming@gmail.com>
2018-12-02 18:33:57 +0800

    completed day1

--- lib/day1.ex
+++ lib/day1.ex
@@ -1,13 +1,31 @@
 defmodule AoC.Day1 do
   def first do
+    read_changes
+    |> Enum.reduce(0, &iterate_change/2)
+  end
+
+  def second(freq \\ 0, freq_list \\ []) do
+    result =
+      Enum.reduce_while(read_changes, {freq, freq_list}, fn c, {f0, fs} ->
+        f1 = iterate_change(c, f0)
+        if f1 in fs, do: {:halt, f1}, else: {:cont, {f1, [f1 | fs]}}
+      end)
+
+    if is_tuple(result) do
+      second(elem(result, 0), elem(result, 1))
+    else
+      result
+    end
+  end
+
+  defp read_changes do
     File.read!("#{System.cwd()}/priv/changes")
     |> String.trim()
     |> String.split("\n")
-    |> Enum.reduce(0, fn c, acc ->
-      {str_action, str_delta} = String.split_at(c, 1)
-      action = String.to_atom(str_action)
-      delta = String.to_integer(str_delta)
-      apply(Kernel, action, [acc, delta])
-    end)
+  end
+
+  defp iterate_change(change, freq) do
+    {str_action, str_delta} = String.split_at(change, 1)
+    apply(Kernel, String.to_atom(str_action), [freq, String.to_integer(str_delta)])
   end
 end
