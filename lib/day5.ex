7679a33c125f814742ed024c463eb4349b75e2e9
Mickey <poverwhelming@gmail.com>
2018-12-05 14:49:26 +0800

    stuck on day5 part1

---
+++ lib/day5.ex
@@ -0,0 +1,20 @@
+defmodule Day5 do
+  @polymer_path "#{System.cwd()}/priv/day5_polymers"
+
+  def part1 do
+    @polymer_path
+    |> File.read!
+    |> String.to_charlist
+    |> run_reaction
+    |> Enum.count
+  end
+
+  def run_reaction(charlist0) do
+    charlist0
+    |> Enum.reverse
+    |> Enum.reduce([], &reduce_units/2)
+  end
+
+  defp reduce_units(char, [h | t]) when abs(char - h) == 32, do: t
+  defp reduce_units(char, acc), do: [char | acc]
+end
