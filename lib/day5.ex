606ef37ca1a8f8bbc7380421a91dcf0015fb1698
Mickey <poverwhelming@gmail.com>
2018-12-05 17:19:13 +0800

    solved part2

--- lib/day5.ex
+++ lib/day5.ex
@@ -3,17 +3,42 @@ defmodule Day5 do
 
   def part1 do
     @polymer_path
-    |> File.read!
-    |> String.trim
-    |> String.to_charlist
+    |> polymer_charlist
     |> run_reaction
-    |> Enum.count
+    |> Enum.count()
+  end
+
+  def part2 do
+    polymer =
+      @polymer_path
+      |> polymer_charlist
+
+    polymer
+    |> Enum.map(fn
+      u when u >= ?a -> u - 32
+      u -> u
+    end)
+    |> Enum.uniq()
+    |> Enum.map(fn u ->
+      polymer
+      |> Enum.reject(&(&1 in [u, u + 32]))
+      |> run_reaction
+      |> Enum.count()
+    end)
+    |> Enum.min()
   end
 
   def run_reaction(charlist0) do
     charlist0
-    |> Enum.reverse
+    |> Enum.reverse()
     |> Enum.reduce([], &reduce_units/2)
+  end
+
+  def polymer_charlist(path) do
+    path
+    |> File.read!()
+    |> String.trim()
+    |> String.to_charlist()
   end
 
   defp reduce_units(char, [h | t]) when abs(char - h) == 32, do: t
